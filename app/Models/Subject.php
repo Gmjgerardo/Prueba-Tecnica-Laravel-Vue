6c4e52d5a9c44562c84adabd22160c25fd52c79b
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-02-04 16:15:22 -0600

    Modify Subject model by guarantee that each teacher has only one subject

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Subject extends Model
@@ -10,6 +11,10 @@ class Subject extends Model
     protected $fillable = [
         'title'
     ];
+
+    public function teacher() : BelongsTo {
+        return $this->belongsTo(Teacher::class, 'teacher_id', 'id');
+    }
 
     public function students(): HasMany {
         return $this->hasMany(Student::class);

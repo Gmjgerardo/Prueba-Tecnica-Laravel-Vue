d2e94be0d37d42471d1735afe478a335ca0b77f4
José Gerardo Gutiérrez Martínez <106112451+Gmjgerardo@users.noreply.github.com>
2025-02-04 13:55:06 -0600

    Adding migrations

---
+++ database/migrations/2025_02_04_183135_create_subjects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            
+            $table->foreignId('teacher_id')
+                    ->references('id')
+                    ->on('teachers')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('subjects', function (Blueprint $table) {
+            $table->dropForeign('teacher_id');
+        });
+
+        Schema::dropIfExists('subjects');
+    }
+};
